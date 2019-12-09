e49a3a095bc22316c2af0511240a28918ffac298
TinyClock <609132714@qq.com>
2019-12-09 09:57:46 +0800

    commit

--- src/cn/ouc/ccf201909/CharPaint.java
+++ src/cn/ouc/ccf201909/CharPaint.java
@@ -1,5 +1,8 @@
 package cn.ouc.ccf201909;
 
+
+import java.util.Scanner;
+
 /**
  * @description:
  * @author: Chuansheng Zhong
@@ -7,5 +10,15 @@ package cn.ouc.ccf201909;
  **/
 public class CharPaint {
 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m, n;//m 图片的宽，n图片的高
+        int p, q;//p每一小块的宽，q每一小块的高
+        m = sc.nextInt();
+        n = sc.nextInt();
+        p = sc.nextInt();
+        q = sc.nextInt();
 
+        System.out.println("0x1b0x5b0x340x380x320x3b0x310x3b0x320x3b");
+    }
 }
